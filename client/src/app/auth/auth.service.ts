f0c1b6d61fc996fd416ebf39f4f3d58e7cea3d78
FedericoPali <palifederico@yahoo.com>
2026-05-27 11:21:38 -0300

    fix: cambios en el login

--- client/src/app/auth/auth.service.ts
+++ client/src/app/auth/auth.service.ts
@@ -14,10 +14,32 @@ export class AuthService {
       // utilizo firstValueFrom para poder convertir el Observable en promesa, esta herramienta lo que hace es suscribirse al observable, espera a que llegue el primer paquete de datos y una vez pasa eso se desuscribe y transforma el dato en una promesa para poder usar el async y await.
       const respuesta = await firstValueFrom(this.http.post("https://fedebackend.vercel.app/usuarios/login", {identificador: identificador, contraseña: contraseña}));
 
-      return respuesta;
+      return this.controlarResponse(respuesta);
     } catch(e){
       console.error(e);
-      throw e;
+      return this.controlarResponse(e);
     }
   }
+
+      async controlarResponse(response: any): Promise<string | null> {
+        if(response.error){
+            switch (response.error.message){
+                case ("Los datos no coinciden"):
+                    throw new Error("El correo o contraseña son incorrectos"); 
+                        
+                case ("User already registered"):
+                    throw new Error("El correo se encuentra registrado");
+
+                case ("Password should contain at least one character of each: abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ, 0123456789."):
+                    throw new Error("La contraseña debe contener letras y numeros");
+
+                default:
+                    throw new Error("Ocurrio un error: " + response.error.message);
+            }
+        } else {
+
+
+            return response
+        }
+    }
 }
--- client/src/app/auth/login/login.ts
+++ client/src/app/auth/login/login.ts
@@ -1,6 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -11,6 +11,10 @@ import { AuthService } from '../auth.service';
 })
 export class Login {
   authS = inject(AuthService)
+  router = inject(Router)
+
+  cargando = signal<boolean>(false);
+  mensajeError = signal<string | null>(null);
 
   formulario = new FormGroup({
     email_username: new FormControl("", [Validators.required]),
@@ -21,11 +25,15 @@ export class Login {
 
   async action(){
     try{
+      this.cargando.set(true);
       const respuesta = await this.authS.loginUsuario(this.formulario.value.email_username!, this.formulario.value.password!)
       console.log("inicio de sesion correcto", respuesta);
+      this.router.navigateByUrl("/publicaciones")
+      this.cargando.set(false);
     } catch(e){
-      console.error("fallo al iniciar sesion", e);
-      
+      console.error(e);
+      this.mensajeError.set((e as Error).message);
+      this.cargando.set(false);
     }
   }
 }
