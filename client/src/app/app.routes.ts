a90dcfa94e452143bf259aa2a597068776c04dee
FedericoPali <palifederico@yahoo.com>
2026-05-27 13:43:37 -0300

    feat: ruta equivocada redirige al login

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -20,5 +20,9 @@ export const routes: Routes = [
         path: "",
         redirectTo: "auth",
         pathMatch: 'full'
+    },
+    {
+        path: '**', 
+        redirectTo: 'auth'
     }
 ];
