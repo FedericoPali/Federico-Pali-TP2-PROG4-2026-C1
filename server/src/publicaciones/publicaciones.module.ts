5762b0ba03487d216e2a1f4c446a2c5a8132713f
FedericoPali <palifederico@yahoo.com>
2026-06-02 19:13:59 -0300

    cambios en imports y exports

--- server/src/publicaciones/publicaciones.module.ts
+++ server/src/publicaciones/publicaciones.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
-import { PublicacionesService } from './publicaciones.service';
 import { PublicacionesController } from './publicaciones.controller';
+import { PublicacionesService } from './publicaciones.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Publicaciones, PublicacionesSchema } from './entities/publicaciones.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Publicaciones.name, schema: PublicacionesSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Publicaciones.name, schema: PublicacionesSchema }]),
+    AuthModule,
+  ],
   controllers: [PublicacionesController],
   providers: [PublicacionesService],
 })
