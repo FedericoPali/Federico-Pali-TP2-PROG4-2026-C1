6ffc65cb4fde4fdac511d2967366c37f899c3289
FedericoPali <palifederico@yahoo.com>
2026-06-18 16:48:18 -0300

    feat: resaltar error implementado al completo

--- client/src/app/directivas/resaltar-error.ts
+++ client/src/app/directivas/resaltar-error.ts
@@ -1,20 +1,18 @@
 import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { NgControl } from '@angular/forms';
 
 @Directive({
   selector: '[appResaltarError]',
   standalone: true
 })
 export class ResaltarErrorDirective {
-
-  @Input('appResaltarError') tieneError: boolean = false;
-
-  constructor(private elemento: ElementRef) {}
+  constructor(private elemento: ElementRef, private control: NgControl) {}
 
   @HostListener('blur') alSalir() {
-    if (this.tieneError) {
-      this.elemento.nativeElement.style.border = '2px solid red';
-    } else {
-      this.elemento.nativeElement.style.border = 'none';
-    }
+      if (this.control.invalid && this.control.touched) {
+          this.elemento.nativeElement.style.border = '2px solid red';
+      } else {
+          this.elemento.nativeElement.style.border = '1px solid #cbd5e1';
+      }
   }
 }
